cfc45df658fa7889c5ae6a0233dd72f18a3a239e
PatMithi <Patrickmithi@gmail.com>
2022-11-02 15:40:21 +0000

    list-additions

---
+++ src/components/meetups/Meetuplist.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './Meetuplist.module.css';
+
+function MeetupList (props) {
+    return (
+        <ul className={classes.list}></ul>
+    );
+}
+
+export default MeetupList;
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,8 +1,33 @@
 import React from "react";
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'This is a first meetup',
+        image: '',
+        address: '97 Clough Road, Sheffield',
+        description: 'This is a first, amazing meetup which you definitely should not miss!'        
+    },
+
+    {
+        id: 'm2',
+        title: 'This is a second meetup',
+        image: '',
+        address: '97 Clough Road, Sheffield',
+        description: 'This is a first, amazing meetup which you definitely should not miss!' 
+    }
+];
 
 function AllMeetupsPage () {
     return (
-        <div>All Meetups Page</div>
+        <section>
+            <h1>All Meetups</h1>
+            <ul>
+                {DUMMY_DATA.map((meetup) => {
+                    return <li key={meetup.id}>{meetup.title}</li>;
+                })}
+            </ul>
+            
+        </section>
     );
 }
 
